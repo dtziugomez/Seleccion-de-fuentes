b15dc344a4b3b488886db13f03fbb3ff5071e4e6
DINDTZIU\Deyvi Tziu <dtziugomez@gmail.com>
2017-10-16 18:35:03 -0500

    seleccion de fuentes

---
+++ seleccion de fuentes/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace seleccion_de_fuentes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboFuentes = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "seleccion de fuentes";
+            // 
+            // cboFuentes
+            // 
+            this.cboFuentes.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.cboFuentes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboFuentes.FormattingEnabled = true;
+            this.cboFuentes.ItemHeight = 25;
+            this.cboFuentes.Location = new System.Drawing.Point(134, 80);
+            this.cboFuentes.Name = "cboFuentes";
+            this.cboFuentes.Size = new System.Drawing.Size(172, 31);
+            this.cboFuentes.TabIndex = 1;
+            this.cboFuentes.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.cboFuentes_DrawItem);
+            this.cboFuentes.SelectedIndexChanged += new System.EventHandler(this.cboFuentes_SelectedIndexChanged);
+            this.cboFuentes.FontChanged += new System.EventHandler(this.Form1_Load);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(235, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "texto de ejemplo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(131, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "selecciona el tipo tamaño de fuente";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "12",
+            "14",
+            "16",
+            "18",
+            "20",
+            "22",
+            "24",
+            "26",
+            "28",
+            "30"});
+            this.comboBox1.Location = new System.Drawing.Point(134, 176);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(163, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 371);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboFuentes);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboFuentes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ seleccion de fuentes/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace seleccion_de_fuentes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboFuentes.Items.AddRange(FontFamily.Families);
+            
+        }
+        private FontStyle GetFontStyle(FontFamily ff)
+        {
+            FontStyle fontStyle = FontStyle.Regular;
+            if (!ff.IsStyleAvailable(FontStyle.Regular))
+                fontStyle = FontStyle.Italic;
+            if (!ff.IsStyleAvailable(FontStyle.Italic))
+                fontStyle = FontStyle.Bold;
+            return fontStyle;
+
+
+        }
+
+        private void cboFuentes_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            ComboBox cboFuentes = (ComboBox)sender;
+            if (e.Index == -1)
+                return;
+            if (sender == null)
+                return;
+            FontFamily fontFamily = (FontFamily)cboFuentes.Items[e.Index];
+            Font fuente = new Font(fontFamily.Name, 12, GetFontStyle(fontFamily));
+            e.DrawBackground();
+            e.DrawFocusRectangle();
+            Graphics g = e.Graphics;
+            g.DrawString(fontFamily.Name, fuente, new SolidBrush(e.ForeColor), e.Bounds.X, e.Bounds.Y + 4);
+        }
+
+        private void cboFuentes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FontFamily fontFamily = (FontFamily)cboFuentes.SelectedItem;
+            label2.Font = new Font(fontFamily.Name, label1.Font.Size, GetFontStyle(fontFamily));
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label2.Font = new Font(label2.Font.Name, int.Parse(comboBox1.SelectedItem.ToString()), label2.Font.Style);
+            
+        }
+    }
+}
